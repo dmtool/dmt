902d7e8b04a065168eaca9aa8586bb548f8ec83b
Cory Perkins <Link2cory@gmail.com>
2015-06-24 22:24:39 -0700

    added a basic User model

--- account/models.py
+++ account/models.py
@@ -1,2 +1,7 @@
 from django.db import models
 
+class User(models.Model):
+	first_name = models.SlugField(max_length=45)
+	last_name  = models.SlugField(max_length=45)
+	Username   = models.CharField(max_length=45)
+	email      = models.EmailField()
